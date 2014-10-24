040caf47f0e944704cefa994a162010b0a71019d
Aesen Vismea <aesen.vismea@gmail.com>
2014-10-24 15:33:21 -0400

    Solid white cube edition.

--- src/blue/lapis/tar/TarResearch.java
+++ src/blue/lapis/tar/TarResearch.java
@@ -2,11 +2,14 @@ package blue.lapis.tar;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.ByteBuffer;
 
 import javax.imageio.ImageIO;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -32,15 +35,92 @@ public class TarResearch {
 			init();
 			BufferedImage skin = ImageIO.read(new URL("https://s3.amazonaws.com/MinecraftSkins/"+player+".png"));
 			BufferedImage head = skin.getSubimage(0, 0, 32, 16);
-			upload(skin, 1);
-			draw(1);
+			upload(head);
+			draw();
+			BufferedImage img = readPixels();
+			ImageIO.write(img, "png", new File(player+".png"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	private static void upload(BufferedImage skin, int glId) {
+	private static BufferedImage readPixels() {
+		GL11.glReadBuffer(GL11.GL_FRONT);
+		ByteBuffer buf = BufferUtils.createByteBuffer(width * height * 4);
+		GL11.glReadPixels(0, 0, width, height, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		  
+		for(int x = 0; x < width; x++)  {
+			for(int y = 0; y < height; y++) {
+				int i = (x + (width * y)) * 4;
+				int r = buf.get(i) & 0xFF;
+				int g = buf.get(i + 1) & 0xFF;
+				int b = buf.get(i + 2) & 0xFF;
+				int a = buf.get(i + 3) & 0xFF;
+				img.setRGB(x, height - (y + 1), (a << 24) | (r << 16) | (g << 8) | b);
+			}
+		}
+		return img;
+	}
+	private static float quadRot = 45;
+	private static void draw() {
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+        GL11.glLoadIdentity();
+        
+        GL11.glLoadIdentity();
+        GL11.glTranslatef(0,0,-5);
+        GL11.glRotatef(quadRot,0f,1.0f,0f);
+        GL11.glColor3f(1, 1, 1);
+        GL11.glBegin(GL11.GL_QUADS);
+        	GL11.glTexCoord2f(0f, 0f);
+            GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+            GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+            GL11.glTexCoord2f(1f, 0f);
+            GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+            GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
+            GL11.glTexCoord2f(1f, 1f);
+            GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+            GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+            GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+
+            GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+            GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+
+            GL11.glVertex3f(-1.0f, 1.0f, 1.0f);
+            GL11.glVertex3f(-1.0f, 1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f,-1.0f);
+            GL11.glVertex3f(-1.0f,-1.0f, 1.0f);
+
+            GL11.glVertex3f( 1.0f, 1.0f,-1.0f);
+            GL11.glVertex3f( 1.0f, 1.0f, 1.0f);
+            GL11.glVertex3f( 1.0f,-1.0f, 1.0f);
+            GL11.glVertex3f( 1.0f,-1.0f,-1.0f);
+        GL11.glEnd();
+	}
+	private static void upload(BufferedImage img) {
+		int[] pixels = new int[img.getWidth()*img.getHeight()];
+		img.getRGB(0, 0, img.getWidth(), img.getHeight(), pixels, 0, img.getWidth());
+		ByteBuffer buf = BufferUtils.createByteBuffer(img.getWidth() * img.getHeight() * 4);
+		for (int y = 0; y < img.getHeight(); y++) {
+			for (int x = 0; x < img.getWidth(); x++) {
+				int pixel = pixels[y*img.getWidth()+x];
+				buf.put((byte) ((pixel >> 16) & 0xFF));
+				buf.put((byte) ((pixel >> 8) & 0xFF));
+                buf.put((byte) (pixel & 0xFF));
+                buf.put((byte) ((pixel >> 24) & 0xFF));
+			}
+		}
+		buf.flip();
 		
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, img.getWidth(), img.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
 	}
 	private static void createBuffer() throws Exception {
 		buffer = new Pbuffer(width, height, new PixelFormat(), null, null);
@@ -58,7 +138,6 @@ public class TarResearch {
 
     private static void initGL() {
         GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glShadeModel(GL11.GL_SMOOTH);
         GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         GL11.glClearDepth(1.0);
         GL11.glEnable(GL11.GL_DEPTH_TEST);

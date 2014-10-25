d3dfd9dbad2f95cfceff478c5e85dc6b5456c6d9
Aesen Vismea <aesen.vismea@gmail.com>
2014-10-24 21:47:02 -0400

    Shadow, default orientation is from the front now

--- src/main/java/blue/lapis/tar/TarResearch.java
+++ src/main/java/blue/lapis/tar/TarResearch.java
@@ -1,9 +1,12 @@
 package blue.lapis.tar;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import javax.imageio.ImageIO;
@@ -16,34 +19,79 @@ import org.lwjgl.util.glu.GLU;
 
 public class TarResearch {
 	// make these switches at some point
-	public static final int width = 256;
-	public static final int height = 256;
+	public static final int supersampling = 4;
+	public static final int width = 256*supersampling;
+	public static final int height = 256*supersampling;
 	private static Pbuffer buffer;
+	private static FloatBuffer lightPosition;
+	private static FloatBuffer lightAmbient;
 	
 	public static void main(String[] args) {
-		String player;
-		if (args.length > 0) {
-			player = args[0];
-		} else {
-			player = "Aesen_";
+		if (args.length == 0) {
+			args = new String[] { "Aesen_" };
 		}
-		System.out.println("Creating avatar for "+player);
-		try {
-			init();
-			BufferedImage skin = ImageIO.read(new URL("https://s3.amazonaws.com/MinecraftSkins/"+player+".png"));
-			BufferedImage head = skin.getSubimage(0, 0, 32, 16);
-			ImageIO.write(head, "png", new File(player+".head.png"));
-			BufferedImage helm = skin.getSubimage(32, 0, 32, 16);
-			ImageIO.write(helm, "png", new File(player+".helm.png"));
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-			draw(upload(head, 1), 1.0f);
-			draw(upload(helm, 2), 1.1f);
-			BufferedImage img = readPixels();
-			ImageIO.write(img, "png", new File(player+".png"));
-			cleanup();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
+		for (String player : args) {
+			System.out.println("Creating avatar for "+player);
+			try {
+				init();
+				BufferedImage skin = ImageIO.read(new URL("https://s3.amazonaws.com/MinecraftSkins/"+player+".png"));
+				BufferedImage head = skin.getSubimage(0, 0, 32, 16);
+				ImageIO.write(head, "png", new File(player+".head.png"));
+				BufferedImage helm = skin.getSubimage(32, 0, 32, 16);
+				ImageIO.write(helm, "png", new File(player+".helm.png"));
+				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+				lightPosition = BufferUtils.createFloatBuffer(4);
+			    lightPosition.mark();
+			    lightPosition.put(-4f);
+			    lightPosition.put(2f);
+			    lightPosition.put(1f);
+			    lightPosition.put(100f);
+			    lightPosition.reset();
+			    
+			    lightAmbient = BufferUtils.createFloatBuffer(4);
+			    lightAmbient.mark();
+			    lightAmbient.put(3.0f);
+			    lightAmbient.put(3.0f);
+			    lightAmbient.put(3.0f);
+			    lightAmbient.put(1f);
+			    lightAmbient.reset();
+				upload(head, 1);
+				upload(helm, 2);
+				
+				GL11.glEnable(GL11.GL_BLEND);
+				GL11.glDisable(GL11.GL_TEXTURE_2D);
+				GL11.glPushMatrix();
+					GL11.glTranslatef(0f, -0.95f, -0.45f);
+					int count = 10;
+					for (int i = 0; i < count; i++) {
+						GL11.glTranslatef(0f, -0.01f, 0f);
+						GL11.glColor4f(0, 0, 0, (1-(i/(float)count))/2f);
+						draw(1.02f, 0.01f, 1.02f);
+					}
+				GL11.glPopMatrix();
+	
+				GL11.glEnable(GL11.GL_TEXTURE_2D);
+				GL11.glEnable(GL11.GL_LIGHTING);
+			    GL11.glEnable(GL11.GL_LIGHT0);
+	
+			    GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+				GL11.glColor3f(1, 1, 1);
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, 1);
+					draw(1.0f, 1.0f, 1.0f);
+				GL11.glBindTexture(GL11.GL_TEXTURE_2D, 2);
+					draw(1.05f, 1.05f, 1.05f);
+				
+				BufferedImage img = readPixels();
+				BufferedImage out = new BufferedImage(width/supersampling, height/supersampling, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D gout = out.createGraphics();
+				gout.drawImage(img.getScaledInstance(img.getWidth()/supersampling, img.getHeight()/supersampling, Image.SCALE_SMOOTH), 0, 0, null);
+				gout.dispose();
+				ImageIO.write(out, "png", new File(player+".png"));
+				cleanup();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
 		}
 	}
 	private static BufferedImage readPixels() {
@@ -64,46 +112,48 @@ public class TarResearch {
 		}
 		return img;
 	}
-	private static float quadRot = 45;
-	private static void draw(int id, float s) throws Exception {
-		GL11.glLoadIdentity();
-		GL11.glTranslatef(0,0.25f,-5);
-		GL11.glRotatef(quadRot,0f,1.0f,0f);
-		GL11.glRotatef(20,1.0f,0f,1.0f);
-		GL11.glColor3f(1, 1, 1);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
-		GL11.glBegin(GL11.GL_QUADS);
-			// Front
-			GL11.glTexCoord2f( 0.25f, 1.00f ); GL11.glVertex3f( -1.0f*s, -1.0f*s, 1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 1.00f ); GL11.glVertex3f(  1.0f*s, -1.0f*s, 1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f(  1.0f*s,  1.0f*s, 1.0f*s );
-			GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f( -1.0f*s,  1.0f*s, 1.0f*s );
-			// Back
-			GL11.glTexCoord2f( 0.75f, 1.00f ); GL11.glVertex3f( -1.0f*s, -1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 1.00f, 1.00f ); GL11.glVertex3f( -1.0f*s,  1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 1.00f, 0.50f ); GL11.glVertex3f(  1.0f*s,  1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*s, -1.0f*s, -1.0f*s );
-			// Top
-			GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f( -1.0f*s,  1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f( -1.0f*s,  1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 0.00f ); GL11.glVertex3f(  1.0f*s,  1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.25f, 0.00f ); GL11.glVertex3f(  1.0f*s,  1.0f*s, -1.0f*s );
-			// Bottom
-			GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f( -1.0f*s, -1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*s, -1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.75f, 0.00f ); GL11.glVertex3f(  1.0f*s, -1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 0.00f ); GL11.glVertex3f( -1.0f*s, -1.0f*s,  1.0f*s );
-			// Left
-			GL11.glTexCoord2f( 0.50f, 1.00f ); GL11.glVertex3f(  1.0f*s, -1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.75f, 1.00f ); GL11.glVertex3f(  1.0f*s,  1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*s,  1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f(  1.0f*s, -1.0f*s,  1.0f*s );
-			// Right
-			GL11.glTexCoord2f( 0.00f, 1.00f ); GL11.glVertex3f( -1.0f*s, -1.0f*s, -1.0f*s );
-			GL11.glTexCoord2f( 0.25f, 1.00f ); GL11.glVertex3f( -1.0f*s, -1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f( -1.0f*s,  1.0f*s,  1.0f*s );
-			GL11.glTexCoord2f( 0.00f, 0.50f ); GL11.glVertex3f( -1.0f*s,  1.0f*s, -1.0f*s );
-		GL11.glEnd();
+	private static float quadRot = 0;
+	private static void draw(float xScale, float yScale, float zScale) throws Exception {
+		GL11.glPushMatrix();
+			GL11.glTranslatef(0,0.25f,-5);
+			GL11.glRotatef(quadRot,0f,1.0f,0f);
+			GL11.glRotatef(20,1.0f,0f,0.0f);
+			GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, lightPosition);
+			GL11.glLight(GL11.GL_LIGHT0, GL11.GL_AMBIENT, lightAmbient);
+			GL11.glBegin(GL11.GL_QUADS);
+				GL11.glNormal3f(0, 0, -1f);
+				// Front
+				GL11.glTexCoord2f( 0.25f, 1.00f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale, 1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 1.00f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale, 1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale, 1.0f*zScale);
+				GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale, 1.0f*zScale);
+				// Back
+				GL11.glTexCoord2f( 1.00f, 1.00f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 1.00f, 0.50f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.75f, 1.00f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				// Top
+				GL11.glTexCoord2f( 0.50f, 0.00f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.25f, 0.00f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+				// Bottom
+				GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.75f, 0.00f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 0.00f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale,  1.0f*zScale);
+				// Left
+				GL11.glTexCoord2f( 0.75f, 1.00f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.75f, 0.50f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 0.50f ); GL11.glVertex3f(  1.0f*xScale,  1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.50f, 1.00f ); GL11.glVertex3f(  1.0f*xScale, -1.0f*yScale,  1.0f*zScale);
+				// Right
+				GL11.glTexCoord2f( 0.00f, 1.00f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale, -1.0f*zScale);
+				GL11.glTexCoord2f( 0.25f, 1.00f ); GL11.glVertex3f( -1.0f*xScale, -1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.25f, 0.50f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale,  1.0f*zScale);
+				GL11.glTexCoord2f( 0.00f, 0.50f ); GL11.glVertex3f( -1.0f*xScale,  1.0f*yScale, -1.0f*zScale);
+			GL11.glEnd();
+		GL11.glPopMatrix();
 	}
 	private static int upload(BufferedImage img, int id) {
 		int[] pixels = new int[img.getWidth()*img.getHeight()];
@@ -140,11 +190,9 @@ public class TarResearch {
 	}
 
 	private static void initGL() {
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		GL11.glClearDepth(1.0);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glShadeModel(GL11.GL_SMOOTH);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glDepthFunc(GL11.GL_LEQUAL);
 
